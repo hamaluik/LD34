6d36e01b77596b8e19ca4b167f2b1f02f9e7e831
Kenton Hamaluik <kenton@hamaluik.com>
2015-12-11 19:42:53 -0700

    Added ideas

---
+++ IDEAS.md
@@ -0,0 +1,9 @@
+# THEME
+
+The theme for LD34 is...
+
+Two button controls + growing!
+
+# Ideas!
+
+Written on paper. Will upload later.
--- src/Play.hx
+++ src/Play.hx
@@ -27,42 +27,5 @@ class Play extends Scene {
 
 	override public function onLoad(_) {
 		Log.info("Loading play scene..");
-
-		/*Promise.when(
-			tusk.defaults.Materials.loadParticlesUntextured()
-		).then(function(particlesMat:Material) {
-			this.useProcessor(new MaterialProcessor());
-			this.useProcessor(new Camera2DProcessor());
-			this.useProcessor(new TransformProcessor());
-			this.useProcessor(new ParticleSystemProcessor(new Vec4(0.25, 0.25, 0.25, 1), true));
-
-			// create the camera
-			var w:Float = Tusk.instance.app.window.width;
-			var h:Float = Tusk.instance.app.window.height;
-			entities.push(new Entity(this, 'Camera', [
-				new TransformComponent(),
-				new Camera2DComponent(new Vec2(w, h) / -2.0, new Vec2(w, h) / 2.0, -100, 100)
-			]));
-
-			// create the particle system
-			var psc:ParticleSystemComponent = new ParticleSystemComponent(1000, 10);
-			psc.customUpdater = function(particle:Particle, pc:ParticleSystemComponent, dt:Float) {
-				// accelerate it due to gravity
-				particle.vel.x = Math.sin(2 * Math.PI * particle.t * 0.2 + (4 * Math.PI * particle.seed)) * 32;
-				particle.vel.y = -40;
-				particle.vel.z = 0;
-
-				// move the particle along
-				particle.lastPos.set(particle.pos.x, particle.pos.y, particle.pos.z);
-				particle.pos.x += particle.vel.x * dt;
-				particle.pos.y += particle.vel.y * dt;
-				particle.pos.z += particle.vel.z * dt;
-			}
-			entities.push(new Entity(this, 'Particle System', [
-				new TransformComponent(new Vec3(0, Tusk.instance.app.window.height / 2, 0), Quat.identity(), new Vec3(2, 2, 2)),
-				psc,
-				new MaterialComponent(particlesMat.path)
-			]));
-		});*/
 	}
 }
