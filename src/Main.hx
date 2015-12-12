40ad0170b8a42afdfd8192c8a7600a99fdde9e9e
Kenton Hamaluik <kenton@hamaluik.com>
2015-12-12 16:49:21 -0700

    Added promise-based game loop for endless minigames!

--- src/Main.hx
+++ src/Main.hx
@@ -6,7 +6,9 @@ import tusk.events.*;
 import tusk.resources.Texture;
 import tusk.macros.ComponentIndexer;
 
+import promhx.Deferred;
 import promhx.Promise;
+import promhx.Stream;
 
 import haxe.ds.StringMap;
 import tusk.defaults.scenes.TechScreen;
@@ -20,12 +22,29 @@ class Main extends Game {
 		return 900;
 	}
 
+	private var minigameDone:Deferred<Scene> = new Deferred<Scene>();
+	private function pickRandomLevel() {
+		var dice:Int = tusk.math.Random.int(0, 0);
+		var scene:Scene = switch(dice) {
+			case 0: new minigames.BottleRocket();
+			default: new minigames.BottleRocket();
+		}
+		Tusk.pushScene(scene).then(function(scene:Scene) {
+			Tusk.removeScene(scene);
+			minigameDone.resolve(scene);
+		});
+	}
+
 	override public function setup() {
 		Log.info('Setting up game...');
 		/*Tusk.pushScene(new tusk.defaults.scenes.SplashScreen()).pipe(function(scene:tusk.Scene) {
 			Tusk.removeScene(scene);
 			return Tusk.pushScene(new minigames.BottleRocket());
 		});*/
-		Tusk.pushScene(new minigames.BottleRocket());
+		//Tusk.pushScene(new minigames.BottleRocket());
+		pickRandomLevel();
+		Stream.whenever(minigameDone.stream()).then(function(_) {
+			pickRandomLevel();
+		});
 	}
 }
